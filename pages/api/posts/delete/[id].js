64e2216c89db38175f4145e6ea3cb623d56c1ba6
Sujaruu <ytfuga02@gmail.com>
2022-07-12 20:19:02 +0700

    add api

---
+++ pages/api/posts/delete/[id].js
@@ -0,0 +1,12 @@
+import db from "../../../../libs/db";
+
+export default async function handler(req, res) {
+  if (req.method !== "DELETE") return res.status(405).end();
+
+  const { id } = req.query;
+
+  const deleteRow = await db("posts").where({ id: id }).del();
+
+  res.status(200);
+  res.json({ message: "data was deleted successfully" });
+}
