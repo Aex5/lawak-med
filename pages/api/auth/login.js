d5e396842abb3439cf3c3b2934cb05249e1bde81
Sujaruu <ytfuga02@gmail.com>
2022-07-14 16:27:11 +0700

    auth

---
+++ pages/api/auth/login.js
@@ -0,0 +1,31 @@
+import db from "../../../libs/db";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") return res.status(405).end();
+
+  const { email, password } = req.body;
+
+  const checkUser = await db("users").where({ email: email }).first();
+  if (!checkUser) {
+    return res.status(401);
+  }
+
+  console.log(checkUser.id);
+
+  //   untuk ngecheck password yang ada di database dengan password yang diinputkan sama atau tidak
+  const checkPassword = await bcrypt.compare(password, checkUser.password);
+
+  const token = jwt.sign(
+    {
+      id: checkUser.id,
+      email: checkUser.email,
+    },
+    "secret123",
+    { expiresIn: "3h" }
+  );
+
+  res.status(200);
+  res.json({ message: "login succsessfully" });
+}
