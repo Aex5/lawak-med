22f48659998efa8a8327d55b2eebeb837ffa8b53
Sujaruu <ytfuga02@gmail.com>
2022-07-17 17:24:23 +0700

    styling

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,29 @@
-import { RiMenu4Fill } from "react-icons/ri";
+import { RiMenu4Fill, RiCloseLine } from "react-icons/ri";
+import { useState } from "react";
 
 export default function Nav() {
+  const [nav, setNav] = useState(false);
+
+  function handlerNav() {
+    setNav(!nav);
+  }
+
   return (
     <div>
-      <div className="w-full h-16 bg-cyan-800 text-white">
-        <div>
-          <div className="flex items-center justify-between px-4">
-            <h3>App</h3>
+      <div className="w-full text-slate-600">
+        <div className="">
+          {/* mobile mneu */}
+          <div
+            className={
+              !nav
+                ? "hidden"
+                : "fixed w-[200px] h-screen flex flex-col items-center justify-start right-0 bg-slate-300 "
+            }
+          >
+            <h3 className="text-4xl py-20">App</h3>
+            {/* menu */}
             <div>
-              <ul>
+              <ul className="flex flex-col gap-3">
                 <li>
                   <a href="/">Posts</a>
                 </li>
@@ -17,9 +32,12 @@ export default function Nav() {
                 </li>
               </ul>
             </div>
-            <div>
-              <RiMenu4Fill />
-            </div>
+            <div></div>
+          </div>
+          <div className="absolute right-0 top-5 px-4">
+            <button onClick={handlerNav} className="text-xl">
+              {nav ? <RiCloseLine /> : <RiMenu4Fill />}
+            </button>
           </div>
         </div>
       </div>
